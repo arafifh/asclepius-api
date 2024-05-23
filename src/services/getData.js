11b0944b44a34e4d89bf0f88e25d739d285a9e77
arafifh <rafifhkmtr@gmail.com>
2024-05-23 18:06:09 +0700

    fix: change collection to predictions

--- src/services/getData.js
+++ src/services/getData.js
@@ -2,8 +2,8 @@ const { Firestore } = require('@google-cloud/firestore');
 
 async function getData() {
     const firestore = new Firestore();
-    
-    return allData = await firestore.collection('Predictions').get();
+
+    return allData = await firestore.collection('predictions').get();
 }
 
 module.exports = getData;
